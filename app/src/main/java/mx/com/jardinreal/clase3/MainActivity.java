9c7bc7143f5236f635f49dab120d2d496edb81b1
mgoscar2018 <39196914+mgoscar2018@users.noreply.github.com>
2018-05-18 15:08:15 -0500

    Dando funcionalidad a los botones del equipo B

--- app/src/main/java/mx/com/jardinreal/clase3/MainActivity.java
+++ app/src/main/java/mx/com/jardinreal/clase3/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int puntajeA = 0;
+    int puntajeB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // PASTE CODE YOU WANT TO TEST HERE
         displayForTeamA(0);
+        displayForTeamB(0);
     }
 
     public void sumarTresPuntos(View v) {
@@ -31,11 +33,37 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamA(puntajeA);
     }
 
+    /*
+    * Metodos para sumar puntajes al equipo B
+     */
+    public void sumarTresPuntosB(View v) {
+        puntajeB += 3;
+        displayForTeamB(puntajeB);
+    }
+
+    public void sumarDosPuntosB(View v) {
+        puntajeB += 2;
+        displayForTeamB(puntajeB);
+    }
+
+    public void sumarUnPuntoB(View v) {
+        puntajeB += 1;
+        displayForTeamB(puntajeB);
+    }
+
     /**
      * Displays the given score for Team A.
      */
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 }
